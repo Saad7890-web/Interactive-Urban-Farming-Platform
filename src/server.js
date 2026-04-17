097d2eef8869f44469f9dc6102c05114dfdcef23
Saad7890-web <omysaadislam@gmail.com>
2026-04-17 12:18:44 +0600

    added socket server for real time tracking

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { Server } from "socket.io";
 import app from "./app.js";
 import { testDbConnection } from "./config/db.js";
 import env from "./config/env.js";
@@ -6,8 +8,46 @@ async function startServer() {
   try {
     await testDbConnection();
 
-    app.listen(env.port, () => {
+    const server = http.createServer(app);
+
+    const io = new Server(server, {
+      cors: {
+        origin: env.corsOrigin,
+        credentials: true,
+      },
+    });
+
+    app.set("io", io);
+
+    io.on("connection", (socket) => {
+      console.log(`🔌 Socket connected: ${socket.id}`);
+
+      socket.on("join:plant", ({ plantId }) => {
+        if (plantId) {
+          socket.join(`plant:${plantId}`);
+        }
+      });
+
+      socket.on("join:user", ({ userId }) => {
+        if (userId) {
+          socket.join(`user:${userId}`);
+        }
+      });
+
+      socket.on("leave:plant", ({ plantId }) => {
+        if (plantId) {
+          socket.leave(`plant:${plantId}`);
+        }
+      });
+
+      socket.on("disconnect", () => {
+        console.log(`Socket disconnected: ${socket.id}`);
+      });
+    });
+
+    server.listen(env.port, () => {
       console.log(`Server running on port ${env.port}`);
+      console.log(`⚡ Socket.IO enabled`);
     });
   } catch (error) {
     console.error("Failed to start server:", error.message);
